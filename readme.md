4d22cd1ca24e1970c5da4c62f07a62062782d270
Anatoliy Kozlov <toliyansky@gmail.com>
2021-12-17 01:16:43 +0300

    Fully new script

--- readme.md
+++ readme.md
@@ -9,19 +9,3 @@
 
 ### Activate rewind
 For activate rewind - press 'select' button.
-
-## Difference with original source
-1) Removed GUI text with (save_offset highest_save). Because it distracts the user and does not carry valuable information for him.
-2) Add GUI text "<< rewind" when activate rewind.
-3) Max frames for save increase from 1000 to 3000
-
-### Comment from [original source](https://github.com/BillyWM/FCEUX-Lua-Script-Collection/blob/master/rewind.lua)
-```
-Author: Billy Wenge-Murphy (http://billy.wenge-murphy.com/)
-Inspired by, but mostly NOT based on, original rewinder script by Antony Lavelle
-(I couldn't quite follow  your code. Sorry!)
-
-"(C) Antony Lavelle 2009	got_wot@hotmail.com		http://www.the-exp.net"
-"This is my first ever time scripting in Lua, so if you can improve on this idea/code please by all means do and
-redistribute it, just please be nice and include original credits along with your own :)"
-```
--- rewinder.lua
+++ rewinder.lua
@@ -1,54 +1,51 @@
-save_array = {}
-save_offset = 1;
-highest_save = 0; --highest save that has actually been created yet
-rewinds = 0;
-frame_count = 0;
-SAVE_MAX = 3000;
-	--save every __ frames. Stretch your rewind power further!
-	--Small values (1-5) recommended
-SAVE_FREQ = 4;
+MAX_SAVED_STATES = 3000
+
+savedStatesQueue = {}
+framesRewind = 0
+startRewindFrame = 0
 
 while true do
+	local joy = joypad.read(1)
 
-	local joy = joypad.read(1);
-	frame_count = frame_count + 1;
-
-	--do rewinding
 	if (joy["select"]) then
-		rewinds = rewinds + 1;
-		if rewinds < SAVE_MAX then
-			gui.text(0, 200, "<< rewind");
-			save_offset = save_offset - 1;
-			--loop around, only if we've filled the whole array once
-			if save_offset < 1 then
-				if highest_save == SAVE_MAX then save_offset = SAVE_MAX else save_offset = 1 end
-			end
+		framesRewind = framesRewind + 1
 
-			savestate.load(save_array[save_offset]);
+		if (startRewindFrame == 0) then
+			startRewindFrame = table.getn(savedStatesQueue)
+		end
+		if (table.getn(savedStatesQueue) > 2 and (startRewindFrame - 2 - framesRewind) > 1) then
+			save = savedStatesQueue[startRewindFrame - 2 - framesRewind]
+
+			savestate.load(save);
+			table.insert(savedStatesQueue, save)
+
+			table.remove(savedStatesQueue, 1)
+			startRewindFrame = startRewindFrame - 1
+
+			gui.text(0, 50, "<< Rewind (" .. startRewindFrame - 2 - framesRewind .. ")")
 		else
-			--wipe out the array to prevent out-of-order saves from being loaded when we loop
-			save_array = {};
-			save_offset = 1;
-			highest_save = 0;
+			gui.text(0, 50, "No frames to rewind")
+			savestate.load(savedStatesQueue[table.getn(savedStatesQueue)]);
 		end
 	else
-		--make a new save, add it to the collection
-		if frame_count >= SAVE_FREQ then
-			save = savestate.create();
-			savestate.save(save);
-			save_array[save_offset] = save;
-			save_offset = save_offset + 1;
-			frame_count = 0;
+		framesRewind = 0
+		startRewindFrame = 0
+
+		if (table.getn(savedStatesQueue) < MAX_SAVED_STATES) then
+			newSave = savestate.create()
+			savestate.save(newSave)
+
+			table.insert(savedStatesQueue, newSave)
+		else
+			newSave = savestate.create()
+			savestate.save(newSave)
+
+			table.remove(savedStatesQueue, 1)
+			table.insert(savedStatesQueue, newSave)
 		end
 
-		rewinds = 0;
 	end
 
-	--loop around when we go over the limit
-	if save_offset > SAVE_MAX then save_offset = 1 end
-
-	--record the highest seen save offset as we reach it
-	if save_offset > highest_save then highest_save = save_offset end
-
-	emu.frameadvance();
+	-- gui.text(0, 10, "SavedStatesQueue size: " .. table.getn(savedStatesQueue))
+	emu.frameadvance()
 end
